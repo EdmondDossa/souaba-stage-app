56379b72fbfb0163c12cd8c4ade231155ad2e1d0
Prosper <hazardprosper227@gmail.com>
2025-09-26 00:08:55 +0100

    update found accomodations

--- app/(main)/found-accomodations/page.js
+++ app/(main)/found-accomodations/page.js
@@ -98,7 +98,7 @@ const FoundProducts = () => {
 
   return (
     <div className="flex items-start mb-10">
-      <div className="w-1/2 pl-20 max-h-screen overflow-y-scroll">
+      <div className="w-1/2 pl-20 max-h-screen">
         <h1 className="font-montserrat-bold text-gray-700 text-2xl mt-10">
           10 résultats trouvés
         </h1>
@@ -120,7 +120,7 @@ const FoundProducts = () => {
             <FilterSideBar onFilterUpdate={()=>{}} />
           </div>
         </div>
-        <div className="mt-10 gap-y-10 flex flex-col justify-end me-15">
+        <div className="mt-10 max-h-screen  gap-y-10 flex flex-col justify-end me-15">
           {Properties.map((property, index) => (
             <PropertyCard
               key={index}
@@ -140,9 +140,9 @@ const FoundProducts = () => {
           ))}
         </div>
       </div>
-      <aside className="w-1/2">
+      <aside className="w-1/2 h-[100vh-100px]">
         <Image
-          className="w-full"
+          className="w-full h-full"
           src={foundAccomodityMap}
           alt="found products map"
         />
--- components/ui/common/FormSteps.jsx
+++ components/ui/common/FormSteps.jsx
@@ -1,14 +1,15 @@
 "use client";
+import { Tooltip } from "react-tooltip";
 
 const FormSteps = ({ steps, currentStep }) => {
   const width = (1 / steps.length) * 60 + "vw";
   const progressBar = (
-    <section className="flex w-full items-center justify-center">
+    <section className="flex w-full items-center justify-center mt-3">
       {steps.map((step, index) => {
         return (
-          <div key={step.name} className="flex items-center">
+          <div id={step.name.split(" ")[0]} key={step.name} className="flex items-center">
             <div
-              className={`relative h-1 text-center mx-0.5 rounded-sm w-[40px] ${
+              className={`relative cursor-pointer h-1 text-center mx-0.5 rounded-sm w-[40px] ${
                 currentStep >= index ? "bg-primary" : "bg-gray-300"
               } ${
                 currentStep === index
@@ -16,6 +17,7 @@ const FormSteps = ({ steps, currentStep }) => {
                   : ""
               } `}
             ></div>
+            <Tooltip  style={{backgroundColor:"#f3f4f6", color:"black"}} place="bottom" className="bg-white text-black" anchorSelect={`#${step.name.split(" ")[0]}`} content={step.name} />
           </div>
         );
       })}
@@ -23,9 +25,12 @@ const FormSteps = ({ steps, currentStep }) => {
   );
   return (
     <div className="flex flex-col justify-center items-center mt-10">
-      <h4 className="font-montserrat-bold text-lg"> {steps[currentStep].name} </h4>
+      <h4 className="font-montserrat-bold text-lg">
+        {" "}
+        {steps[currentStep].name}{" "}
+      </h4>
       <span className="font-montserrat-medium mb-4 block">
-        Etape {currentStep+1}/{steps.length}
+        Etape {currentStep + 1}/{steps.length}
       </span>
       {progressBar}
     </div>
