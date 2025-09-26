5f5003e815d8ff80cea338d6fce9635087a83685
Prosper <hazardprosper227@gmail.com>
2025-09-26 17:42:31 +0100

    update details

--- components/ui/common/PropertyCard.jsx
+++ components/ui/common/PropertyCard.jsx
@@ -29,6 +29,7 @@ export default function PropertyCard({
   ellipsis = 0,
   showPrice = true,
   ownerInfo = null,
+  imageContainerClassName="",
   ...rest
 }) {
   return (
@@ -40,12 +41,12 @@ export default function PropertyCard({
     >
       {/* Image Container */}
       <div
-        className="w-full h-96 bg-cover rounded-xl"
+        className={`w-full h-96 bg-cover rounded-xl ${ imageContainerClassName ?? ""}`}
         style={{ backgroundImage: `url(${imageUrl})` }}
       >
         <div className="relative card-body h-full w-full rounded-xl bg-black/50 bg-img">
           {/* Price Tag */}
-          <div className="absolute  bottom-1 left-4 w-full">
+          <div className="absolute bottom-1 left-4 w-full">
             <div className="flex justify-between items-center">
               <strong className="font-montserrat-medium text-white block text-[17px] font-bold">
                 {showPrice && price}
